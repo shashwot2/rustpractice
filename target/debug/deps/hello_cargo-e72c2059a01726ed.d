6d276493fad944b0a0e256e5de0bf85895bcd87d
Shashwot KC <shashwot_07@hotmail.com>
2024-05-05 11:12:05 -0400

    Add lesson 2

---
+++ target/debug/deps/hello_cargo-e72c2059a01726ed.d
@@ -0,0 +1,5 @@
+/home/kc/Code/RustPractice/target/debug/deps/libhello_cargo-e72c2059a01726ed.rmeta: src/main.rs
+
+/home/kc/Code/RustPractice/target/debug/deps/hello_cargo-e72c2059a01726ed.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/kc/Code/RustPractice/target/debug/hello_cargo: /home/kc/Code/RustPractice/src/main.rs
